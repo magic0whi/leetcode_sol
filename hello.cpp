084f7810987f8ed752d934b6ba1db37599fd7923
proteus <lollipop.studio.cn@gmail.com>
2024-08-26 20:30:31 +0800

    feat(CPP): Review & Rewrite (6/?): Perfect forwarding
    Signed-off-by: proteus <lollipop.studio.cn@gmail.com>

--- hello.cpp
+++ hello.cpp
@@ -1,54 +1,26 @@
 // import leetcode;
 // import linalg;
 import std;
-class String {
-private:
-  std::size_t m_size;
-  char* m_data;
-public:
-  String() = default;
-  String(std::string_view const str) : m_size(str.size()), m_data(new char[str.size()]) {
-    std::println("Created!");
-    std::copy_n(str.cbegin(), m_size, m_data);
-  }
-  // Copy constructor
-  String(String const& other) : m_size(other.m_size), m_data(new char[other.m_size]) {
-    std::println("Copied!");
-    std::copy_n(other.m_data, m_size, m_data);
-  }
-  String(String&& older) noexcept : m_size(older.m_size), m_data{older.m_data} {
-    std::println("Moved!");
-    // There presents a problem: when the old one gets deleted, it's going to
-    // delete the m_data here as well. So the major thing that we need to do is to
-    // make the old one point to nothing.
-    older.m_size = 0, older.m_data = nullptr;
-  }
-  String& operator=(String&& older) noexcept { // define move assignment
-    if (this != &older) {
-      delete[] m_data; // Move assignment assumes that there already exists data in currently class, we shall clean the current class
-      std::println("Moved!\n");
-      m_size = older.m_size, m_data = older.m_data;
-      older.m_size = 0, older.m_data = nullptr;
-    }
-    return *this;
-  }
-  ~String() { std::println("Destroyed!"), delete[] m_data; }
-  void print() {
-    for (std::size_t i{}; i < m_size; i++) std::cout << m_data[i];
-    std::println("");
-  }
-};
+template <typename T> consteval std::string_view type_name() noexcept {
+  std::string_view capt{std::source_location::current().function_name()};
+  return {capt.cbegin() + capt.find('=') + 2, capt.cend() - 1};
+}
 
+auto f(auto&&) { std::println("f(T&&) matched"); }
+auto f(auto const&) { std::println("f(T const&) matched"); }
+
+template <typename T> void wrapper(T&& arg) {
+  f<T>(arg); // Forwarding reference will let it invokes f(T&&), so we need explicitly specify f<T>.
+  f<T>(std::forward<T>(arg)); // Perfect forwarding
+}
 int main() {
-  String apple{"Apple"}, dest;
-
-  std::print("Apple: "), apple.print();
-  std::print("Dest: "), dest.print();
-
-  dest = std::move(apple); // With 'std::move()' invokes move assignment
-
-  std::print("Apple: "), apple.print();
-  std::print("Dest: "), dest.print();
+  wrapper(1);
+  int a{1};
+  wrapper(a); // Compiler will deduce to 'int&', equivalent to wrapper(auto(a))
+  wrapper(std::move(a));
+  std::println(
+    "{}, {}", type_name<decltype(std::move(a))>(), // std::decay gets the type as if passing to function arguments
+    type_name<std::decay_t<decltype(std::move(a))>>()); // (array of 'T' gets 'T*'; function types gets function pointer type; remove cv-qualifier).
 }
 
 // int main() { leetcode_run(); }
